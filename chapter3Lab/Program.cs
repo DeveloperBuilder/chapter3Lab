08faef20680cb27b31da01ce9a1de86843621dfa
LSLIM <LSLIM@LS>
2018-02-02 20:06:50 +0100

    Made no changes

--- chapter3Lab/Program.cs
+++ chapter3Lab/Program.cs
@@ -10,6 +10,18 @@ namespace chapter3Lab
     {
         static void Main(string[] args)
         {
+            int a = 122;
+            int b = 10;
+            int c = a + b;
+            double d = ((double) a + (double) b) / 2;
+            double e = ((double) a * (double) b) / 100;
+            char f = (char)a;
+            Console.WriteLine(a + "\n" + b);    // Display the numbers
+            Console.WriteLine(c);               // Add the numbers and display the sum of the values
+            Console.WriteLine(d);               // Calculate the average and display
+            Console.WriteLine(e);               // Calculate the percentage on ons number by another and display
+            Console.WriteLine(f);               // Convert the numbers to char values and display
+            Console.ReadLine();
         }
     }
 }
